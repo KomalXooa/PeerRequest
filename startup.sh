05c305c595dfd6d86c5e25db92f10aae3220666d
KomalXooa <49023759+KomalXooa@users.noreply.github.com>
2019-07-01 15:53:09 +0530

    Created startup.sh

---
+++ startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo apt update -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update -y
+sudo apt install docker-ce -y
+sudo apt install nodejs -y
+sudo apt install npm -y
+npm init -y
+npm install express -y
+npm install shelljs -y
