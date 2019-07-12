43e8cb446f83d3e50a3cc6853d6667545e1064fa
KomalXooa <49023759+KomalXooa@users.noreply.github.com>
2019-07-12 18:49:35 +0530

    Update peersetup.sh

--- peersetup.sh
+++ peersetup.sh
@@ -7,7 +7,10 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update -y
 sudo apt install docker-ce -y
-sudo apt install nodejs -y
+#echo 'Installing Nodejs'
+sudo curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt-get install nodejs -y
 sudo apt install npm -y
 sudo npm init -y
 sudo npm install express -y
@@ -16,9 +19,10 @@ sudo apt-get install git-core
 sudo git clone https://github.com/Xooa/external_peer_cloud_backend.git external_peer
 cd external_peer
 cd backend
-sudo npm install --save
+sudo npm install 
 sudo nohup node new_server.js &
 cd ..
+echo "Starting"
 cd frontend
-sudo npm install --save
-sudo npm start Y
+sudo npm install 
+sudo nohup npm start &
