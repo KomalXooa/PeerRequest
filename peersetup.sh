7bf7b9c32d7b598593271c4c84d8c85f981e050f
KomalXooa <49023759+KomalXooa@users.noreply.github.com>
2019-07-12 16:29:27 +0530

    Update peersetup.sh

--- peersetup.sh
+++ peersetup.sh
@@ -7,7 +7,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update -y
 sudo apt install docker-ce -y
-sudo apt install nodejs -y
+sudo curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt-get install nodejs
 sudo apt install npm -y
 npm init -y
 npm install express -y
